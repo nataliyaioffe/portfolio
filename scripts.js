07761acb71d1b6d6643f53ab5f83769f982dcff5
Nataliya Ioffe <nxioffe@gmail.com>
2018-12-18 00:23:35 -0500

    Styles, links to sites, project copy, CSS org

--- scripts.js
+++ scripts.js
@@ -10,7 +10,21 @@ $(function() {
 app.init = function() {
   // window.onscroll();
   app.menu();
-};
+  
+
+  $(".fa-angle-double-down").on("click", function(e) {
+    if (app.scrollCount < 5) {
+      app.scrollCount++
+    } else {
+      app.scrollCount = 1;
+    }
+    console.log(app.scrollCount);
+    $("html, body").animate({ scrollTop: $(`.scroll-${app.scrollCount}`).offset().top}, 1000);
+  });
+
+}
+
+app.scrollCount = 0;
 
 app.menu = function() {
   $(".hamburger").on("click", function() {
